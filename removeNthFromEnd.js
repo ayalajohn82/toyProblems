f19238461007d9f2c3803fa68a858cb6a36905a8
ayala <deus@ubuntu.ubuntu-domain>
2018-02-08 14:32:18 -0800

    problem setup

---
+++ removeNthFromEnd.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+class ListNode {
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = (head, n) => {
+  
+};
+
+//  1->2->3->4->5
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+const result = removeNthFromEnd();
+console.log(result);
