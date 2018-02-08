6b1bf1ecda96b14a210646d85f5b10cf5f5ea000
ayala <deus@ubuntu.ubuntu-domain>
2018-02-08 14:13:19 -0800

    levelOrderTree

--- levelOrderTree.js
+++ levelOrderTree.js
@@ -5,10 +5,53 @@
  *     this.left = this.right = null;
  * }
  */
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-    
+const levelOrder = (root) => {
+  if(root === null) {
+    return [];
+  }
+  let levelOrder = [];
+  let queue = [root];
+  while(queue.length > 0) {
+    let order = [];
+    const length = queue.length;
+    for(let i = 0; i < length; i++) {
+      let current = queue.shift(); 
+      if(current.left !== null) {
+        queue.push(current.left);
+      }
+      if(current.right !== null) {
+        queue.push(current.right);
+      }
+      order.push(current.val);
+    }
+    levelOrder.push(order);
+  }
+  return levelOrder;
 };
+
+//   3
+//  / \
+// 9  20
+//   /  \
+//  15   7
+let root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7); 
+console.log(root);
+
+const result = levelOrder(root);
+console.log(result);
