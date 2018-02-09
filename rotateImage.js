5d0bde2d558a17acc8d4f430f29adc3fa7a3bddc
ayala <deus@ubuntu.ubuntu-domain>
2018-02-09 15:06:42 -0800

    rotateImaage

--- rotateImage.js
+++ rotateImage.js
@@ -18,7 +18,12 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 const rotate = (matrix) => {
-    
+  let clonedArray = JSON.parse(JSON.stringify(matrix));
+  for(var i = 0; i < matrix.length;  i++) {
+    for(var j = 0; j < matrix.length; j++) {
+      matrix[i][j] = clonedArray[matrix.length - 1 - j][i];
+    }
+  }
 };
 
 const result = rotate([[1,2,3],[4,5,6],[7,8,9]]);
