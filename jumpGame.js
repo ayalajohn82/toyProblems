d7200ec5d9686460ea51d9b5124cdc13bd940f8f
ayala <deus@ubuntu.ubuntu-domain>
2018-02-10 09:05:08 -0800

    jumpGame

--- jumpGame.js
+++ jumpGame.js
@@ -10,7 +10,24 @@
  * @return {boolean}
  */
 const canJump = (nums) => {
-    
+  const length = nums.length - 1;
+  let front = 0; 
+  let back = 0;
+  let steps = 0;
+
+  while(back < length) {
+    steps += 1;
+    let max = back + 1;
+    for(var i = front; i < back+ 1; i++) {
+      if(i + nums[i] >= length) {
+        return steps;
+      }
+      max = Math.max(max, i + nums[i]);
+    }
+    front = back + 1;
+    back = max;
+  }
+  return steps;
 };
 
 const result = canJump([2,3,1,1,4]);
